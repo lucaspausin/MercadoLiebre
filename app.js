08d2aaf63607ff9eeda245cc7c5165fd2cf10105
lucaspausin <lucaspausin@gmail.com>
2023-07-28 16:06:56 -0300

    agregando Forms, paginas de login y register.

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/views/home.html");
 });
 
+app.get("/login", (req, res) => {
+	res.sendFile(__dirname + "/views/login.html");
+});
+
+app.get("/register", (req, res) => {
+	res.sendFile(__dirname + "/views/register.html");
+});
+
 app.listen(3000, () => {
 	console.log("Servidor corriendo en el puerto 3000");
 });
